4e29db1b6e558ffb71543988bc392a672183c2ae
Luiggy Macias <ferrinluiggy@gmail.com>
2021-07-20 11:43:59 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//var sass = require('sass');
+import './scss/header.scss';
+
+const Header = () => {
+	return (
+		<header>
+			helo
+		</header>
+		)
+}
+
+export default Header
