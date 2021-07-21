827b72f24ee86b71ced9f5973a165599f64ff65a
Luiggy Macias <ferrinluiggy@gmail.com>
2021-07-21 10:50:00 -0500

    selecting an option

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useReducer, useEffect } from 'react';
 //components
 import Header from './components/Header';
 import Main from './components/Main';
 
+const initialState = {
+  isLoading: false,
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'OPTION_SELECTED':
+      return {
+        ...state,
+        hand: action.payload,
+        isLoading: true,
+      }
+    case 'MACHINE_OPTION':
+      return {
+        ...state,
+        machineHand: action.payload,
+      }
+
+  }
+}
+
 function App() {
-  const [score, setScore] = useState(12)
+  const [score, setScore] = useState(12);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const {hand, machineHand, isLoading} = state
+  const options = ['paper', 'scissors', 'rock'];
+
+  const selectAnOption = e => {
+    console.log(e.target.id)
+    dispatch({
+      type: 'OPTION_SELECTED',
+      payload: options[e.target.id]
+    })
+  }
+  useEffect(() => {
+    console.log(`rendered`)
+  }, [hand])
+
   return (
     <div className="App">
       <Header score={score} />
-      <Main />
+      <Main
+       selectAnOption={selectAnOption}
+       isLoading={isLoading}
+       options={options}
+       hand={hand}
+        />
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './scss/main.scss'
 
-const Main = () => {
+const Main = ({selectAnOption, isLoading, hand, machineHand}) => {
+	let userClass = hand === 'paper'
+		? 'paper_hand hand'
+		: hand === 'scissors'
+		? 'scissors_hand hand'
+		: 'rock_hand hand'
+
+	let machineClass = machineHand === 'paper'
+		? 'paper_hand hand'
+		: machineHand === 'scissors'
+		? 'scissors_hand hand'
+		: 'rock_hand hand'
+
+		console.log(machineHand + 'dddd')
 	return (
 		<main>
-			<button className='paper_hand hand'>ss</button>
-			<button className='scissors_hand hand'>ss</button>
-			<button className='rock_hand hand'>ss</button>
+			{!isLoading 
+				? (
+					<>
+						<button id='0' className='paper_hand hand' onClick={selectAnOption} ></button>
+						<button id='1' className='scissors_hand hand'></button>
+						<button id='2' className='rock_hand hand'></button>
+					</>
+					) : (
+					<>
+						<button id='1' className={userClass}></button>
+						<button id='2' className={machineClass}></button>
+					</>
+					)}
 		</main>
 		)
 }
